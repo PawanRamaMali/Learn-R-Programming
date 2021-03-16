14115fcb7d512af623ac2fbedc9a6b64a7023d64
Pawan Rama Mali <prm@outlook.in>
2021-03-16 21:30:10 +0530

    added data wrangling

--- 999 Business Analysis/1 Sales Analysis/sales_analysis.R
+++ 999 Business Analysis/1 Sales Analysis/sales_analysis.R
@@ -1,8 +1,12 @@
-#install.packages('readxl')
+# Required Libraries ----
 
+#install.packages('readxl')
 library(readxl)
+library(dplyr)
 
 # Read Files ----
+
+?read_excel
 
 bikes_tbl <- read_excel('data/bikes.xlsx')
 
@@ -13,5 +17,25 @@ orderlines_tbl <- read_excel('data/orderlines.xlsx')
 # View Data ----
 
 bikes_tbl
+
 bikeshops_tbl
+
 orderlines_tbl
+
+# Join Data ----
+
+?left_join
+
+left_join(orderlines_tbl,
+          bikes_tbl ,
+          by = c('product.id' = 'bike.id'))
+
+
+bike_orderlines_joined_tbl <- orderlines_tbl %>%
+    left_join(bikes_tbl,
+              by = c("product.id" = "bike.id")) %>%
+    left_join(bikeshops_tbl,
+              by = c('customer.id' = 'bikeshop.id'))
+
+
+bike_orderlines_joined_tbl %>% glimpse()
