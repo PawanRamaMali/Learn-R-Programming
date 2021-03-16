f877b82cf06733c9102d5530028810019181e3ae
Pawan Rama Mali <prm@outlook.in>
2021-03-16 21:12:51 +0530

    Joined data tables

--- 999 Business Analysis/1 Sales Analysis/sales_analysis.R
+++ 999 Business Analysis/1 Sales Analysis/sales_analysis.R
@@ -2,9 +2,16 @@
 
 library(readxl)
 
+# Read Files ----
 
 bikes_tbl <- read_excel('data/bikes.xlsx')
 
 bikeshops_tbl <- read_excel('data/bikeshops.xlsx')
 
 orderlines_tbl <- read_excel('data/orderlines.xlsx')
+
+# View Data ----
+
+bikes_tbl
+bikeshops_tbl
+orderlines_tbl
